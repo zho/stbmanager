15e735c3019098972333b73567be7756fe3910d3
zho <joandi@gmail.com>
2020-09-26 17:18:01 +0700

    initial-commit

---
+++ src/main/java/id/kings/stbmanager/StbmanagerApplication.java
@@ -0,0 +1,13 @@
+package id.kings.stbmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StbmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StbmanagerApplication.class, args);
+    }
+
+}
